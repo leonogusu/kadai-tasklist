638154ada1ebfadcbc449b15c6b5f391e6efb949
leon.ogusu <figer97s.i.x.x@gmail.com>
2019-11-09 18:27:17 +0000

    users-assosiation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:update, :create, :destroy]
     
     def index
         @tasks = Task.all.page(params[:page])
@@ -14,11 +16,12 @@ class TasksController < ApplicationController
     
     def create
         @task = Task.new(task_params)
-        
+        @task = current_user.tasks.build(task_params)
         if @task.save
             flash[:success] = 'タスクが正常に投稿されました'
             redirect_to @task
         else
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
             flash.now[:danger] = 'タスクが投稿されませんでした'
             render :new
         end
@@ -53,6 +56,13 @@ class TasksController < ApplicationController
  def task_params
     params.require(:task).permit(:content, :status)
  end
+ 
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 
 end
 
